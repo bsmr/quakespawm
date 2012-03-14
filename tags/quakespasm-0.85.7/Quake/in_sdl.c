e10fdf2bb020db3d54128bce6d51add889e9f5cf
sezero <sezero@af15c1b1-3010-417e-b628-4374ebc0bcbd>
2012-03-14 07:00:27 +0000

    tagged revision 646 as quakespasm-0.85.7

---
+++ tags/quakespasm-0.85.7/Quake/in_sdl.c
@@ -0,0 +1,590 @@
+/*
+Copyright (C) 1996-2001 Id Software, Inc.
+Copyright (C) 2002-2005 John Fitzgibbons and others
+Copyright (C) 2007-2008 Kristian Duske
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#include "quakedef.h"
+#include "SDL.h"
+
+#ifdef __APPLE__
+/* Mouse acceleration needs to be disabled on OS X */
+#define MACOS_X_ACCELERATION_HACK
+#endif
+
+#ifdef MACOS_X_ACCELERATION_HACK
+#include <IOKit/IOTypes.h>
+#include <IOKit/hidsystem/IOHIDLib.h>
+#include <IOKit/hidsystem/IOHIDParameter.h>
+#include <IOKit/hidsystem/event_status_driver.h>
+#endif
+
+static keydest_t	prev_key_dest;
+
+static qboolean	no_mouse = false;
+
+static int buttonremap[] =
+{
+	K_MOUSE1,
+	K_MOUSE3,	/* right button		*/
+	K_MOUSE2,	/* middle button	*/
+	K_MWHEELUP,
+	K_MWHEELDOWN,
+	K_MOUSE4,
+	K_MOUSE5
+};
+
+/* mouse variables */
+cvar_t	m_filter = {"m_filter","0",CVAR_NONE};
+
+/* total accumulated mouse movement since last frame,
+ * gets updated from the main game loop via IN_MouseMove */
+static int	total_dx, total_dy = 0;
+
+static int FilterMouseEvents (const SDL_Event *event)
+{
+	switch (event->type)
+	{
+	case SDL_MOUSEMOTION:
+	case SDL_MOUSEBUTTONDOWN:
+	case SDL_MOUSEBUTTONUP:
+		return 0;
+	}
+
+	return 1;
+}
+
+#ifdef MACOS_X_ACCELERATION_HACK
+static cvar_t in_disablemacosxmouseaccel = {"in_disablemacosxmouseaccel", "1", CVAR_ARCHIVE};
+static double originalMouseSpeed = -1.0;
+
+static io_connect_t IN_GetIOHandle(void)
+{
+	io_connect_t iohandle = MACH_PORT_NULL;
+	io_service_t iohidsystem = MACH_PORT_NULL;
+	mach_port_t masterport;
+	kern_return_t status;
+
+	status = IOMasterPort(MACH_PORT_NULL, &masterport);
+	if (status != KERN_SUCCESS)
+		return 0;
+
+	iohidsystem = IORegistryEntryFromPath(masterport, kIOServicePlane ":/IOResources/IOHIDSystem");
+	if (!iohidsystem)
+		return 0;
+
+	status = IOServiceOpen(iohidsystem, mach_task_self(), kIOHIDParamConnectType, &iohandle);
+	IOObjectRelease(iohidsystem);
+
+	return iohandle;
+}
+
+static void IN_DisableOSXMouseAccel (void)
+{
+	io_connect_t mouseDev = IN_GetIOHandle();
+	if (mouseDev != 0)
+	{
+		if (IOHIDGetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), &originalMouseSpeed) == kIOReturnSuccess)
+		{
+			if (IOHIDSetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), -1.0) != kIOReturnSuccess)
+			{
+				Cvar_Set("in_disablemacosxmouseaccel", "0");
+				Con_Printf("WARNING: Could not disable mouse acceleration (failed at IOHIDSetAccelerationWithKey).\n");
+			}
+		}
+		else
+		{
+			Cvar_Set("in_disablemacosxmouseaccel", "0");
+			Con_Printf("WARNING: Could not disable mouse acceleration (failed at IOHIDGetAccelerationWithKey).\n");
+		}
+		IOServiceClose(mouseDev);
+	}
+	else
+	{
+		Cvar_Set("in_disablemacosxmouseaccel", "0");
+		Con_Printf("WARNING: Could not disable mouse acceleration (failed at IO_GetIOHandle).\n");
+	}
+}
+
+static void IN_ReenableOSXMouseAccel (void)
+{
+	io_connect_t mouseDev = IN_GetIOHandle();
+	if (mouseDev != 0)
+	{
+		if (IOHIDSetAccelerationWithKey(mouseDev, CFSTR(kIOHIDMouseAccelerationType), originalMouseSpeed) != kIOReturnSuccess)
+			Con_Printf("WARNING: Could not re-enable mouse acceleration (failed at IOHIDSetAccelerationWithKey).\n");
+		IOServiceClose(mouseDev);
+	}
+	else
+	{
+		Con_Printf("WARNING: Could not re-enable mouse acceleration (failed at IO_GetIOHandle).\n");
+	}
+	originalMouseSpeed = -1;
+}
+#endif /* MACOS_X_ACCELERATION_HACK */
+
+
+void IN_Activate (void)
+{
+	if (no_mouse)
+		return;
+
+#ifdef MACOS_X_ACCELERATION_HACK
+	/* Save the status of mouse acceleration */
+	if (originalMouseSpeed == -1 && in_disablemacosxmouseaccel.value)
+		IN_DisableOSXMouseAccel();
+#endif
+
+	if (SDL_WM_GrabInput(SDL_GRAB_QUERY) != SDL_GRAB_ON)
+	{
+		SDL_WM_GrabInput(SDL_GRAB_ON);
+		if (SDL_WM_GrabInput(SDL_GRAB_QUERY) != SDL_GRAB_ON)
+			Con_Printf("WARNING: SDL_WM_GrabInput(SDL_GRAB_ON) failed.\n");
+	}
+
+	if (SDL_ShowCursor(SDL_QUERY) != SDL_DISABLE)
+	{
+		SDL_ShowCursor(SDL_DISABLE);
+		if (SDL_ShowCursor(SDL_QUERY) != SDL_DISABLE)
+			Con_Printf("WARNING: SDL_ShowCursor(SDL_DISABLE) failed.\n");
+	}
+
+	if (SDL_GetEventFilter() != NULL)
+		SDL_SetEventFilter(NULL);
+
+	total_dx = 0;
+	total_dy = 0;
+}
+
+void IN_Deactivate (qboolean free_cursor)
+{
+	if (no_mouse)
+		return;
+
+#ifdef MACOS_X_ACCELERATION_HACK
+	if (originalMouseSpeed != -1)
+		IN_ReenableOSXMouseAccel();
+#endif
+
+	if (free_cursor)
+	{
+		if (SDL_WM_GrabInput(SDL_GRAB_QUERY) != SDL_GRAB_OFF)
+		{
+			SDL_WM_GrabInput(SDL_GRAB_OFF);
+			if (SDL_WM_GrabInput(SDL_GRAB_QUERY) != SDL_GRAB_OFF)
+				Con_Printf("WARNING: SDL_WM_GrabInput(SDL_GRAB_OFF) failed.\n");
+		}
+
+		if (SDL_ShowCursor(SDL_QUERY) != SDL_ENABLE)
+		{
+			SDL_ShowCursor(SDL_ENABLE);
+			if (SDL_ShowCursor(SDL_QUERY) != SDL_ENABLE)
+				Con_Printf("WARNING: SDL_ShowCursor(SDL_ENABLE) failed.\n");
+		}
+	}
+
+	/* discard all mouse events when input is deactivated */
+	if (SDL_GetEventFilter() != FilterMouseEvents)
+		SDL_SetEventFilter(FilterMouseEvents);
+}
+
+void IN_Init (void)
+{
+	SDL_EnableUNICODE (0); /* frame updates will change this as key_dest changes */
+	if (SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL) == -1)
+		Con_Printf("Warning: SDL_EnableKeyRepeat() failed.\n");
+
+	if (safemode || COM_CheckParm("-nomouse"))
+	{
+		no_mouse = true;
+		/* discard all mouse events when input is deactivated */
+		SDL_SetEventFilter(FilterMouseEvents);
+	}
+
+#ifdef MACOS_X_ACCELERATION_HACK
+	Cvar_RegisterVariable(&in_disablemacosxmouseaccel);
+#endif
+
+	IN_Activate();
+}
+
+void IN_Shutdown (void)
+{
+	IN_Deactivate(true);
+}
+
+void IN_Commands (void)
+{
+/* TODO: implement this for joystick support */
+}
+
+extern cvar_t cl_maxpitch; /* johnfitz -- variable pitch clamping */
+extern cvar_t cl_minpitch; /* johnfitz -- variable pitch clamping */
+
+
+void IN_MouseMove(int dx, int dy)
+{
+	total_dx += dx;
+	total_dy += dy;
+}
+
+void IN_Move (usercmd_t *cmd)
+{
+	int		dmx, dmy;
+
+	/* TODO: fix this
+	if (m_filter.value)
+	{
+		dmx = (2*mx - dmx) * 0.5;
+		dmy = (2*my - dmy) * 0.5;
+	}
+	*/
+
+	dmx = total_dx * sensitivity.value;
+	dmy = total_dy * sensitivity.value;
+
+	total_dx = 0;
+	total_dy = 0;
+
+	if ( (in_strafe.state & 1) || (lookstrafe.value && (in_mlook.state & 1) ))
+		cmd->sidemove += m_side.value * dmx;
+	else
+		cl.viewangles[YAW] -= m_yaw.value * dmx;
+
+	if (in_mlook.state & 1)
+	{
+		if (dmx || dmy)
+			V_StopPitchDrift ();
+	}
+
+	if ( (in_mlook.state & 1) && !(in_strafe.state & 1))
+	{
+		cl.viewangles[PITCH] += m_pitch.value * dmy;
+		/* johnfitz -- variable pitch clamping */
+		if (cl.viewangles[PITCH] > cl_maxpitch.value)
+			cl.viewangles[PITCH] = cl_maxpitch.value;
+		if (cl.viewangles[PITCH] < cl_minpitch.value)
+			cl.viewangles[PITCH] = cl_minpitch.value;
+	}
+	else
+	{
+		if ((in_strafe.state & 1) && noclip_anglehack)
+			cmd->upmove -= m_forward.value * dmy;
+		else
+			cmd->forwardmove -= m_forward.value * dmy;
+	}
+}
+
+void IN_ClearStates (void)
+{
+}
+
+void IN_SendKeyEvents (void)
+{
+	SDL_Event event;
+	int sym, state;
+	int modstate;
+
+	if (key_dest != prev_key_dest)
+	{
+		SDL_EnableUNICODE((key_dest == key_console || key_dest == key_message));
+		Key_ClearStates();
+		prev_key_dest = key_dest;
+	}
+
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_ACTIVEEVENT:
+			if (event.active.state & (SDL_APPINPUTFOCUS|SDL_APPACTIVE))
+			{
+				if (event.active.gain)
+				{
+				/*	Sys_Printf("FOCUS GAIN\n");*/
+					S_UnblockSound();
+				}
+				else
+				{
+				/*	Sys_Printf("FOCUS LOSS\n");*/
+					S_BlockSound();
+				}
+			}
+			break;
+
+		case SDL_KEYDOWN:
+			if ((event.key.keysym.sym == SDLK_RETURN) &&
+			    (event.key.keysym.mod & KMOD_ALT))
+			{
+				VID_Toggle();
+				break;
+			}
+			else if ((event.key.keysym.sym == SDLK_ESCAPE) &&
+				 (event.key.keysym.mod & KMOD_SHIFT))
+			{
+				Con_ToggleConsole_f();
+				break;
+			}
+
+		case SDL_KEYUP:
+			sym = event.key.keysym.sym;
+			state = event.key.state;
+			modstate = SDL_GetModState();
+
+			switch (key_dest)
+			{
+			case key_game:
+				if ((event.key.keysym.unicode != 0) || (modstate & KMOD_SHIFT))
+				{	/* only use unicode for ~ and ` in game mode */
+					if ((event.key.keysym.unicode & 0xFF80) == 0)
+					{
+						if (((event.key.keysym.unicode & 0x7F) == '`') ||
+						    ((event.key.keysym.unicode & 0x7F) == '~') )
+							sym = event.key.keysym.unicode & 0x7F;
+					}
+				}
+				break;
+			case key_message:
+			case key_console:
+				if ((event.key.keysym.unicode != 0) || (modstate & KMOD_SHIFT))
+				{
+#if defined(__APPLE__) && defined(__MACH__)
+					if (sym == SDLK_BACKSPACE)
+						break;	/* avoid change to SDLK_DELETE */
+#endif	/* Mac OS X */
+#if defined(__QNX__)
+					if ((sym == SDLK_BACKSPACE) || (sym == SDLK_RETURN))
+						break;	/* S.A: fixes QNX weirdness */
+#endif	/* __QNX__ */
+					if ((event.key.keysym.unicode & 0xFF80) == 0)
+						sym = event.key.keysym.unicode & 0x7F;
+					/* else: it's an international character */
+				}
+			/*	printf("You pressed %s (%d) (%c)\n", SDL_GetKeyName(sym), sym, sym);*/
+				break;
+			default:
+				break;
+			}
+
+			switch (sym)
+			{
+			case SDLK_DELETE:
+				sym = K_DEL;
+				break;
+			case SDLK_BACKSPACE:
+				sym = K_BACKSPACE;
+				break;
+			case SDLK_F1:
+				sym = K_F1;
+				break;
+			case SDLK_F2:
+				sym = K_F2;
+				break;
+			case SDLK_F3:
+				sym = K_F3;
+				break;
+			case SDLK_F4:
+				sym = K_F4;
+				break;
+			case SDLK_F5:
+				sym = K_F5;
+				break;
+			case SDLK_F6:
+				sym = K_F6;
+				break;
+			case SDLK_F7:
+				sym = K_F7;
+				break;
+			case SDLK_F8:
+				sym = K_F8;
+				break;
+			case SDLK_F9:
+				sym = K_F9;
+				break;
+			case SDLK_F10:
+				sym = K_F10;
+				break;
+			case SDLK_F11:
+				sym = K_F11;
+				break;
+			case SDLK_F12:
+				sym = K_F12;
+				break;
+			case SDLK_BREAK:
+			case SDLK_PAUSE:
+				sym = K_PAUSE;
+				break;
+			case SDLK_UP:
+				sym = K_UPARROW;
+				break;
+			case SDLK_DOWN:
+				sym = K_DOWNARROW;
+				break;
+			case SDLK_RIGHT:
+				sym = K_RIGHTARROW;
+				break;
+			case SDLK_LEFT:
+				sym = K_LEFTARROW;
+				break;
+			case SDLK_INSERT:
+				sym = K_INS;
+				break;
+			case SDLK_HOME:
+				sym = K_HOME;
+				break;
+			case SDLK_END:
+				sym = K_END;
+				break;
+			case SDLK_PAGEUP:
+				sym = K_PGUP;
+				break;
+			case SDLK_PAGEDOWN:
+				sym = K_PGDN;
+				break;
+			case SDLK_RSHIFT:
+			case SDLK_LSHIFT:
+				sym = K_SHIFT;
+				break;
+			case SDLK_RCTRL:
+			case SDLK_LCTRL:
+				sym = K_CTRL;
+				break;
+			case SDLK_RALT:
+			case SDLK_LALT:
+				sym = K_ALT;
+				break;
+			case SDLK_KP0:
+				if (modstate & KMOD_NUM)
+					sym = K_INS;
+				else
+					sym = SDLK_0;
+				break;
+			case SDLK_KP1:
+				if (modstate & KMOD_NUM)
+					sym = K_END;
+				else
+					sym = SDLK_1;
+				break;
+			case SDLK_KP2:
+				if (modstate & KMOD_NUM)
+					sym = K_DOWNARROW;
+				else
+					sym = SDLK_2;
+				break;
+			case SDLK_KP3:
+				if (modstate & KMOD_NUM)
+					sym = K_PGDN;
+				else
+					sym = SDLK_3;
+				break;
+			case SDLK_KP4:
+				if (modstate & KMOD_NUM)
+					sym = K_LEFTARROW;
+				else
+					sym = SDLK_4;
+				break;
+			case SDLK_KP5:
+				sym = SDLK_5;
+				break;
+			case SDLK_KP6:
+				if (modstate & KMOD_NUM)
+					sym = K_RIGHTARROW;
+				else
+					sym = SDLK_6;
+				break;
+			case SDLK_KP7:
+				if (modstate & KMOD_NUM)
+					sym = K_HOME;
+				else
+					sym = SDLK_7;
+				break;
+			case SDLK_KP8:
+				if (modstate & KMOD_NUM)
+					sym = K_UPARROW;
+				else
+					sym = SDLK_8;
+				break;
+			case SDLK_KP9:
+				if (modstate & KMOD_NUM)
+					sym = K_PGUP;
+				else
+					sym = SDLK_9;
+				break;
+			case SDLK_KP_PERIOD:
+				if (modstate & KMOD_NUM)
+					sym = K_DEL;
+				else
+					sym = SDLK_PERIOD;
+				break;
+			case SDLK_KP_DIVIDE:
+				sym = SDLK_SLASH;
+				break;
+			case SDLK_KP_MULTIPLY:
+				sym = SDLK_ASTERISK;
+				break;
+			case SDLK_KP_MINUS:
+				sym = SDLK_MINUS;
+				break;
+			case SDLK_KP_PLUS:
+				sym = SDLK_PLUS;
+				break;
+			case SDLK_KP_ENTER:
+				sym = SDLK_RETURN;
+				break;
+			case SDLK_KP_EQUALS:
+				sym = SDLK_EQUALS;
+				break;
+			case 178: /* the '²' key */
+				sym = '~';
+				break;
+			}
+			/* If we're not directly handled and still
+			 * above 255, just force it to 0 */
+			if (sym > 255)
+				sym = 0;
+			Key_Event (sym, state);
+			break;
+
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+			if (event.button.button < 1 ||
+			    event.button.button > sizeof(buttonremap) / sizeof(buttonremap[0]))
+			{
+				Con_Printf ("Ignored event for mouse button %d\n",
+							event.button.button);
+				break;
+			}
+			Key_Event(buttonremap[event.button.button - 1], event.button.state == SDL_PRESSED);
+			break;
+
+		case SDL_MOUSEMOTION:
+			IN_MouseMove(event.motion.xrel, event.motion.yrel);
+			break;
+
+		case SDL_QUIT:
+			CL_Disconnect ();
+			Sys_Quit ();
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
---
+++ tags/quakespasm-0.85.7/README.txt
@@ -0,0 +1,396 @@
+  QuakeSpasm
+
+  ____________________________________________________________
+
+  Table of Contents
+
+
+  1. About
+  2. Downloads
+  3. Hints
+     3.1 Music Playback
+
+  4. Compiling
+     4.1 Linux/Unix
+     4.2 Windows
+     4.3 Mac OS X
+
+  5. Changes
+     5.1 Changes in 0.85.7
+     5.2 Changes in 0.85.6
+     5.3 Changes in 0.85.5
+     5.4 Changes in 0.85.4
+     5.5 Changes in 0.85.3
+     5.6 Changes in 0.85.2
+     5.7 Changes in 0.85.1
+
+  6. Todo
+  7. Copyright
+  8. Links
+
+
+  ______________________________________________________________________
+
+
+  Page last edited 12 March 2012
+
+
+  1.  About
+
+  QuakeSpasm is a Quake 1 engine based on the SDL port of FitzQuake.  It
+  includes 64bit CPU support, a new sound driver, several networking
+  fixes and a few graphical niceities.
+
+  <http://quakespasm.sourceforge.net>
+
+
+  2.  Downloads
+
+  o  <http://quakespasm.sourceforge.net/download.htm>
+
+
+  3.  Hints
+
+  Visit the FitzQuake Homepage <http://www.celephais.net/fitzquake> for
+  a full run-down of the engine's commands and variables.
+
+
+  o  To disable some changes, use "quakespasm -fitz"
+
+  o  For different sound drivers use "SDL_AUDIODRIVER=DRIVER
+     ./quakespasm" , where DRIVER may be alsa, dsp, pulse, esd ...
+
+  o  Shift+Escape draws the Console.
+
+  o  From the console, use UP to browse the command line history and TAB
+     to autocomplete command and map names.
+
+  o  There is currently no CD Music volume support. cd_sdl.c needs
+     replacing with cd_linux.c, cd_bsd.c etc..
+
+  o  In windows, alternative CD drives are accessible by "quakespasm
+     -cddev F" (for example)
+
+  o  Quakespasm allows loading new games (mods) on the fly with "game
+     GAMENAME"
+
+
+  3.1.  Music Playback
+
+  Since version 0.85.4, Quakespasm can play back external MP3, OGG and
+  Wave music files.
+
+  o  Tracks should be named like "track02.ogg", "track03.ogg" ... (there
+     is no track01) and placed into "Quake/id1/music".
+
+  o  Unix users may need some extra libraries installed: "libmad" or
+     "libmpg123" for MP3, and "libogg" and "libvorbis" for OGG.
+
+  o  To prevent tracks from being downsampled, use the "-sndspeed"
+     option to set a sufficiently high sample rate.
+
+  o  Use the "-noextmusic" option to disable this feature.
+
+  o  See  <README.music> for more details.
+
+
+  4.  Compiling
+
+  To check-out the latest version of QuakeSpasm, use :
+  svn co https://quakespasm.svn.sourceforge.net/svnroot/quakespasm/trunk
+
+  4.1.  Linux/Unix
+
+  After extracting the source tarball, browse the Makefile and edit the
+  music streaming options, then
+
+  ______________________________________________________________________
+  make
+  cp quakespasm /usr/local/games/quake (for example)
+  ______________________________________________________________________
+
+  Compile time options include
+
+  o  make DEBUG=1 for debugging
+
+  o  make SDL_CONFIG=/PATH/TO/SDL-CONFIG for unusual SDL installations
+
+  Streaming music playback requires "libmad" or "libmpg123" for MP3, and
+  "libogg" and "libvorbis" for OGG files.
+
+  HOME directory support can be enabled via Misc/homedir_0.patch
+
+  The project can also be built with Codeblocks (project files
+  included).
+
+  4.2.  Windows
+
+  The QuakeSpasm developers cross-compile windows binaries using MinGW
+  <http://www.mingw.org> and Mingw-w64 <http://mingw-w64.sf.net>.
+
+  The project can also be built using Visual Studio 2005 (or newer).
+
+  4.3.  Mac OS X
+
+  A Quakespasm App (including program launcher and update framework) can
+  be made using the Xcode template found in the MacOSX directory.
+
+  Alternatively, have a look at Makefile.darwin for more instructions on
+  building from a console.
+
+
+  5.  Changes
+
+
+  5.1.  Changes in 0.85.7
+
+  o  Added support for cross-level demo playback
+
+  o  gl_texturemode is reimplemented as a cvar with a callback and the
+     setting is automatically saved to the config
+
+  o  Fixed execution of external files without a newline at the end
+
+  o  Reduced memory usage during reloading of textures
+
+  o  Fixed compilation on GNU/kFreeBSD (Debian bug #657793)
+
+  o  Fixed backspace key on Mac OS X
+
+  o  Disable mouse acceleration in Mac OS X
+
+  o  Worked around recursive calling of the anisotropic filter callback
+
+  o  Console word wrap and long input line fixes
+
+  o  Verified correct compilation by clang (using v3.0)
+
+  o  Several other small changes mostly invisible to the end-user
+
+
+  5.2.  Changes in 0.85.6
+
+  o  More work for string buffer safety
+
+  o  Reverted v0.85.5 change of not allowing deathmatch and coop cvars
+     to be set at the same time (was reported for possibility of causing
+     compatibility issues with mods)
+
+  o  Several cleanups/changes in the cvar layer
+
+  o  Minor SDL video fixes.
+
+
+  5.3.  Changes in 0.85.5
+
+  o  SDL input driver updated adding native keymap and dead key support
+     to the console
+
+  o  Fixed a crash in net play in maps with extended limits
+
+  o  Verified successful compilation using gcc-4.6.x
+
+  o  Added workaround against GL texture flicker (z fighting),
+     controlled by new cvar 'gl_zfix'
+
+  o  Read video variables early so that a vid_restart isn't necessary
+     after init
+
+  o  mlook and lookspring fixes
+
+  o  Added support for loading external entity files, controlled by new
+     cvar 'external_ents'
+
+  o  Made mp3 playback to allocate system memory instead of zone
+
+  o  Some updates to the progs interpreter code
+
+  o  Fixed r_nolerp_list parsing code of fitzquake
+
+  o  Made sure that deathmatch and coop are not set at the same time
+
+  o  Several code updates from uHexen2 project, several code cleanups.
+
+
+  5.4.  Changes in 0.85.4
+
+  o  Implement music (OGG, MP3, WAV) playback
+
+  o  A better fix for the infamous SV_TouchLinks problem, no more hard
+     lockups with maps such as "whiteroom"
+
+  o  Add support for mouse buttons 4 and 5
+
+  o  Fix the "unalias" console command
+
+  o  Restore the "screen size" menu item
+
+  o  Fixed an erroneous protocol check in the server code
+
+  o  Raised the default zone memory size to 384 kb
+
+  o  Raised the default max_edicts from 1024 to 2048
+
+  o  Revised lit file loading, the lit file must be from the same game
+     directory as the map itself or from a searchpath with a higher
+     priority
+
+  o  Fixed rest of the compiler warnings
+
+  o  Other minor sound and cdaudio updates
+
+
+  5.5.  Changes in 0.85.3
+
+  o  Fix the "-dedicated" option (thanks Oz) and add platform specific
+     networking code (default) rather than SDL_net
+
+  o  Much needed OSX framework stuff from Kristian
+
+  o  Add a persistent history feature (thanks Baker)
+
+  o  Add a slider for scr_sbaralpha, which now defaults to 0.95
+     (slightly transparent, allowing for a nicer status bar)
+
+  o  Allow player messages longer than 32 characters
+
+  o  Sockaddr fix for FreeBSD/OSX/etc networking
+
+  o  Connect status bar size to the scale slider
+
+  o  Include an ISNAN (is not-a-number) fix to catch the occassional
+     quake C bug giving traceline problems
+
+  o  Enumerate options menus
+
+  o  Add a "prev weapon" menu item (from Sander)
+
+  o  Small fix to Sound Block/Unblock on win32
+
+  o  Lots of code fixes (some from uhexen2)
+
+  o  Sys_Error calls Host_Shutdown
+
+  o  Added MS Visual Studio support
+
+  o  Add a "-cd" option to let the CD Player work in dedicated mode, and
+     some other CD tweaks.
+
+
+  5.6.  Changes in 0.85.2
+
+  o  Replace the old "Screen size" slider with a "Scale" slider
+
+  o  Don't constantly open and close condebug log
+
+  o  Heap of C clean-ups
+
+  o  Fix mapname sorting
+
+  o  Alias the "mods" command to "games"
+
+  o  Block/Unblock sound upon focus loss/gain
+
+  o  NAT fix (networking protocol fix)
+
+  o  SDLNet_ResolveHost bug-fix allowing connection to ports other than
+     26000
+
+  o  Bumped array size of sv_main.c::localmodels from 5 to 6 fixing an
+     old fitzquake-0.85 bug which used to cause segfaults depending on
+     the compiler.
+
+  o  Accept commandline options like "+connect ip:port"
+
+  o  Add OSX Makefile (tested?)
+
+
+  5.7.  Changes in 0.85.1
+
+  o  64 bit CPU support
+
+  o  Restructured SDL sound driver
+
+  o  Custom conback
+
+  o  Tweaked the command line completion and added a map/changelevel
+     autocompletion function
+
+  o  Alt+Enter toggles fullscreen
+
+  o  Disable Draw_BeginDisc which causes core dumps when called
+     excessively
+
+  o  Show helpful info on start-up
+
+  o  Include real map name (sv.name) and skill in the status bar
+
+  o  Remove confirm quit dialog
+
+  o  Don't spam the console with PackFile seek requests
+
+  o  Default to window mode
+
+  o  Withdraw console when playing demos
+
+  o  Don't play demos on program init
+
+  o  Default Heapsize is 64meg
+
+  o  Changes to default console alpha, speed
+
+  o  Changes to cvar persistence gl_flashblend (default 0), r_shadow,
+     r_wateralpha, r_dynamic, r_novis
+
+
+  6.  Todo
+
+  o  Add uHexen2's first person camera (and menu item)
+
+  o  Native CD audio support (if desired). cd_sdl.c doesn't have proper
+     volume controls
+
+  o  Test usb keyboards. Do the keypads work? Make the OSX apple key
+     work.
+
+  o  Complete the unix user directories support
+
+
+  7.  Copyright
+
+  o  Quake and Quakespasm are released under the GNU GENERAL PUBLIC
+     LICENSE Version 2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+  o  Quakespasm console background image by AAS, released under the
+     CREATIVE COMMONS PUBLIC LICENSE
+     <http://creativecommons.org/licenses/by/3.0/legalcode>
+
+
+  8.  Links
+
+  o  QuakeSpasm Homepage <http://quakespasm.sourceforge.net>
+
+  o  QuakeSpasm Project page
+     <http://sourceforge.net/projects/quakespasm>
+
+  o  FitzQuake Homepage <http://www.celephais.net/fitzquake>
+
+  o  Sleepwalkr's Original SDL Port
+     <http://www.kristianduske.com/fitzquake>
+
+  o  Baker's 0.85 Source Code
+     <http://quakeone.com/proquake/src_other/fitzquake_sdl_20090510_src_beta_1.zip>
+
+  o  Func Quakespasm forum
+     <http://www.celephais.net/board/view_thread.php?id=60452>
+
+  o  Func SDL Fitzquake forum
+     <http://www.celephais.net/board/view_thread.php?id=60172>
+
+  o  Ozkan's email <mailto:gmail - dot - com - username - sezeroz>
+
+  o  Stevenaaus email <mailto:yahoo - dot - com - username - stevenaaus>
+
+  o  Kristian's email <mailto:gmail - dot - com - username - inveigle>
+
+
